d8ff38e0eda12ad7d9c52c4cabffb1c33be0e442
Paul Sore <paul.sore@gmail.com>
2014-11-06 13:27:43 +0100

    Decoding all netmessages, adding gulp/jsdoc
    Todo : general cleanup before implementing usermessages

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp')
+	, jsdoc = require('gulp-jsdoc');
+
+gulp.task('default', function() {
+  console.log("gulped ?")
+
+  gulp.src("./*.js")
+  	.pipe(jsdoc('./docs'));
+
+});
