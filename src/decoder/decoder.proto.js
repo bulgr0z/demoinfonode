0d3e85434abc82bf5b08ead524faeaffd488e5f2
Paul Sore <paul.sore@gmail.com>
2015-02-21 15:49:39 +0100

    Cleaning up for v0.1

--- src/decoder/decoder.proto.js
+++ src/decoder/decoder.proto.js
@@ -3,24 +3,16 @@ var ProtocolBuffers = require('protocol-buffers');
 var fs = require('fs');
 
 // Proto
-var NetMessageProto = require('../../protobuf/netmessages2.proto.json');
+var NetMessageProto = require('../../protobuf/netmessages.proto.json');
 var UserMessageProto = require('../../protobuf/usermessages.proto.json');
 
 var NetMessageBuilder = new ProtoBuf.Builder();
 NetMessageBuilder.import(NetMessageProto);
-// var NetMessageBuilder = ProtoBuf.loadProtoFile(__dirname+"/../../protobuf/netmessages_public.proto");
 var NetMessage = NetMessageBuilder.build('NetMessages'); // return the messages namespace
 
 var UserMessageBuilder = new ProtoBuf.Builder();
 UserMessageBuilder.import(UserMessageProto);
 var UserMessage = UserMessageBuilder.build();
-
-// var ProtoNetMessage = ProtocolBuffers(fs.readFileSync(__dirname+'/../../protobuf/netmessages_public.proto'));
-
-var parser = new ProtoBuf.DotProto.Parser(fs.readFileSync(__dirname+'/../../protobuf/netmessages_public.proto'));
-var p = parser.parse();
-
-//debugger;
 
 module.exports = {
 	UserMessageBuilder: UserMessageBuilder,
