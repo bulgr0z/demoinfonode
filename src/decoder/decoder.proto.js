26007098cfeebf1ed43ae058ccf2ece8883fa5b2
Paul Sore <paul.sore@gmail.com>
2015-03-21 22:38:53 +0100

    fix loading protobuf when used as a node_module

--- src/decoder/decoder.proto.js
+++ src/decoder/decoder.proto.js
@@ -1,8 +1,9 @@
-var ProtoBuf = require('protobufjs');
+var ProtoBuf = require('protobufjs')
+  , path = require('path');
 
 var CombinedBuilder = ProtoBuf.loadProtoFile({
-	root: process.cwd(),
-	file: 'protobuf/combined.proto'
+  root: path.resolve(__dirname, '../../'),
+  file: 'protobuf/combined.proto'
 });
 var Messages = CombinedBuilder.build('CSGOMessages'); // return the messages namespace
 
