fdced38788a755a35b31066392a57e1db30ec39c
Paul Sore <paul.sore@gmail.com>
2015-03-21 21:38:24 +0100

    move to bitbucket, add modeler to parse gameevents

--- example.js
+++ example.js
@@ -1,18 +1,43 @@
 var Fs = require('fs')
   , PacketParser = require('./src/parser/packetparser.js')
-  , PacketDecoder = require('./src/decoder/decoder.js');
+  , PacketDecoder = require('./src/decoder/decoder.js')
+  , GameEvents = require('./src/modeler/modeler.gameevents.js');
 
-var entrystream = Fs.createReadStream('/path/to/demofile.dem');
+// player_connect_full
+// player_team
+var eventsToDecode = ['player_death', 'player_connect', 'player_disconnect']
+// var eventsToDecode = ['player_death', 'player_team', 'player_connect_full']
+
+var entrystream = Fs.createReadStream('/Volumes/MecanicalHD/Dropbox/dev/csgo/demo/match730_003035043074272133283_0536844275_184.dem');
 var packetParser = new PacketParser();
 var packetDecoder = new PacketDecoder();
-entrystream.pipe(packetParser).pipe(packetDecoder);
+var gameEvents = new GameEvents(eventsToDecode);
+entrystream.pipe(packetParser).pipe(packetDecoder).pipe(gameEvents);
 
 var cmd = {};
 var a = new Date();
+
+packetParser.on('header', function(header) {
+  console.log(header)
+  // process.exit(0)
+})
+
 packetDecoder.on('data', function(packet) {
   // Do stuff with your decoded packet
 	if (!cmd[packet.metadata.cmd]) cmd[packet.metadata.cmd] = 0;
 	cmd[packet.metadata.cmd] += 1;
+	// console.log(JSON.stringify(packet, null, 1))
+
+  var gameevents;
+  packet.messages.forEach(function(msg) {
+    if (msg.messageName === 'CSVCMsg_GameEvent') {
+//      console.log(JSON.stringify(msg, null, 1))
+    }
+  })
+});
+
+gameEvents.on('data', function(data) {
+  console.log('\n\nEVENT', data)
 });
 
 
---
+++ src/modeler/modeler.gameevents.js
@@ -0,0 +1,145 @@
+var Stream = require('stream')
+  , Util = require('util')
+  , _ = require('lodash');
+
+// @param Array.<string> events The events to get
+var GameEvents = function(events) {
+  Stream.Transform.call(this, {objectMode: true});
+
+  this.events = events;
+  this.eventsById = {};
+}
+
+Util.inherits(GameEvents, Stream.Transform);
+module.exports = GameEvents;
+
+GameEvents.prototype._transform = function(packet, encoding, done) {
+
+  var decoded = this.decodeEvents(packet);
+  if (decoded) this.push(decoded);
+
+  return done();
+};
+
+
+GameEvents.prototype.decodeEvents = function(packet) {
+
+  var decoded = {};
+  var hasGameEvent = false;
+
+  packet.messages.forEach(function(msg) {
+
+    if (msg.messageName === 'CSVCMsg_GameEvent') {
+
+      var message = msg.message;
+      if (this.eventsById[message.eventid]) {
+        var eventType = this.eventsById[message.eventid].name;
+
+        var gameevent = this.handleGameEvent(message, this.eventsById[message.eventid].keys);
+
+        if (!decoded[eventType])
+          decoded[eventType] = [];
+
+        decoded[eventType].push(gameevent);
+        hasGameEvent = true;
+      }
+      // msg.message.eventid
+      // this.handleGameEvent(msg.message, )
+      // console.log(JSON.stringify(msg, null, 1))
+      // HANDLE GAME EVENT
+    }
+    
+    if (msg.messageName === 'CSVCMsg_GameEventList') {
+
+      msg.message.descriptors.forEach(function(descriptor) {
+        // if we have to handle this event
+        if (this.events.indexOf(descriptor.name) > -1) {
+          this.eventsById[descriptor.eventid] = descriptor;
+        }
+      }, this);
+    }
+
+  }, this);
+
+  if (hasGameEvent)
+    return decoded;
+
+  return false;
+};
+
+GameEvents.prototype.handleGameEvent = function(message, eventKeys) {
+
+  var decoded = {};
+  message.keys.forEach(function(key, index) {
+    var type = key.type;
+    var name = eventKeys[index].name;
+    var indexes = Object.keys(key);
+    decoded[name] = key[indexes[type]];
+  });
+  return decoded;
+};
+
+// {
+//  "byteSize": 9,
+//  "message": {
+//   "event_name": null,
+//   "eventid": 162,
+//   "keys": [
+//    {
+//     "type": 4,
+//     "val_string": null,
+//     "val_float": null,
+//     "val_long": null,
+//     "val_short": 12,
+//     "val_byte": null,
+//     "val_bool": null,
+//     "val_uint64": null,
+//     "val_wstring": null
+//    }
+//   ]
+//  },
+//  "messageName": "CSVCMsg_GameEvent"
+// }
+// {
+//  "byteSize": 24,
+//  "message": {
+//   "event_name": null,
+//   "eventid": 124,
+//   "keys": [
+//    {
+//     "type": 4,
+//     "val_string": null,
+//     "val_float": null,
+//     "val_long": null,
+//     "val_short": 10,
+//     "val_byte": null,
+//     "val_bool": null,
+//     "val_uint64": null,
+//     "val_wstring": null
+//    },
+//    {
+//     "type": 1,
+//     "val_string": "ak47",
+//     "val_float": null,
+//     "val_long": null,
+//     "val_short": null,
+//     "val_byte": null,
+//     "val_bool": null,
+//     "val_uint64": null,
+//     "val_wstring": null
+//    },
+//    {
+//     "type": 6,
+//     "val_string": null,
+//     "val_float": null,
+//     "val_long": null,
+//     "val_short": null,
+//     "val_byte": null,
+//     "val_bool": false,
+//     "val_uint64": null,
+//     "val_wstring": null
+//    }
+//   ]
+//  },
+//  "messageName": "CSVCMsg_GameEvent"
+// }
